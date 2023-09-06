58b86e472df3c92d7fef65944ee750cc6811e70f
Omkar Yadav <yadomkar@Omkars-MacBook-Pro.local>
2023-09-06 16:10:52 -0700

    question 4 code

--- Homework1/Question4.cpp
+++ Homework1/Question4.cpp
@@ -2,14 +2,37 @@
 // Created by Omkar Yadav on 9/4/23.
 //
 
-// #include<unordered_map>
 #include<iostream>
-// #include<vector>
+#include<unordered_set>
+#include<unordered_map>
+#include<vector>
+#include<stack>
 
 using namespace std;
 
 class Question4 {
+    void dfs(char vertex, unordered_map<char, vector<char>>& graph, unordered_set<char>& visited, stack<char>& topo) {
+        visited.insert(vertex);
 
+        for(char& neighbor: graph[vertex]) {
+            if(visited.find(neighbor) == visited.end()) {
+                dfs(neighbor, graph, visited, topo);
+            }
+        }
+        topo.push(vertex);
+    }
+
+public:
+    stack<char> topologicalSort(unordered_map<char, vector<char>>& graph) {
+        stack<char> topo;
+        unordered_set<char> visited;
+        for(auto& [key, value]: graph){
+            if(visited.find(key) == visited.end()) {
+                dfs(key, graph, visited, topo);
+            }
+        }
+        return topo;
+    }
 };
 
 int main() {
@@ -22,5 +45,20 @@ int main() {
             {'f', {'c', 'd'}},
             {'g', {'d', 'f'}}
     };
+
+    stack<char> topologicalSorted = Question4().topologicalSort(graph);
+
+    while(!topologicalSorted.empty()) {
+        cout << topologicalSorted.top() << " ";
+        topologicalSorted.pop();
+    }
+
     return 0;
 }
+
+/*
+
+ Output:
+ e b g f d c a
+
+ */
